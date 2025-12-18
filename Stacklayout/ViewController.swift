5e421fd33a628721164179ef0ce15bb86e47950a
Vedanshi Mehta <vedanshi.mehta@1rivet.com>
2025-12-18 10:59:54 +0530

    Initial Commit

---
+++ Stacklayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stacklayout
+//
+//  Created by ADMIN on 18/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
