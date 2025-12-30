69c25c4123f318dbc1e23ddeda11226f6a5ed6d9
Vedanshi Mehta <vedanshi.mehta@1rivet.com>
2025-12-30 10:30:55 +0530

    added: eample of navigation controller

---
+++ Navigation_Ex/PinkViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PinkViewController.swift
+//  Navigation_Ex
+//
+//  Created by ADMIN on 29/12/25.
+//
+
+import UIKit
+
+class PinkViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Pink View"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Navigation_Ex/TealViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TealViewController.swift
+//  Navigation_Ex
+//
+//  Created by ADMIN on 29/12/25.
+//
+
+import UIKit
+
+class TealViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       title = "Teal View"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
